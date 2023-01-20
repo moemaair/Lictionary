30314887583b140b6aae1efd64444cb1bf0af7c3
MohamedIbrahim <rageali12@gmail.com>
2023-01-20 19:46:16 +0300

    Map Definations in Meaning dto

--- app/src/main/java/com/moemaair/lictionary/feature_dictionary/data/remote/dto/Meaning.kt
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/data/remote/dto/Meaning.kt
@@ -1,17 +1,14 @@
 package com.moemaair.lictionary.feature_dictionary.data.remote.dto
 
 data class Meaning(
-    val antonyms: List<String>,
     val definitions: List<Definition>,
     val partOfSpeech: String,
-    val synonyms: List<String>
 ){
     fun toMeaning(): Meaning{
         return Meaning(
-            antonyms = antonyms,
-            definitions = definitions,
+            definitions = definitions.map{it.toDefination()},
             partOfSpeech = partOfSpeech,
-            synonyms = synonyms
+
         )
     }
 }
