8d3ff402f8a32d630b12829172c436bb3c483cca
MohamedIbrahim <rageali12@gmail.com>
2023-01-24 10:06:36 +0300

    Add GetWordInfo Usecase for the App's First usecase

---
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.moemaair.lictionary.feature_dictionary.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+}
---
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/domain/use_case/GetWordInfo.kt
@@ -0,0 +1,15 @@
+import com.moemaair.lictionary.core.util.Resource
+import com.moemaair.lictionary.feature_dictionary.domain.repository.WordInfoRepo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetWordInfo (
+    private val repo: WordInfoRepo
+){
+    operator fun invoke(word: String): Flow<Resource<List<WordInfo>>> {
+        if(word.isBlank()) {
+            return flow {  }
+        }
+        return repo.getWordInfo(word)
+    }
+}
