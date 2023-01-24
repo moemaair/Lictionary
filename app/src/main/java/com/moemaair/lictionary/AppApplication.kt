86fafb6bd27c92af68fbab4b95d378b77c650845
MohamedIbrahim <rageali12@gmail.com>
2023-01-24 10:15:12 +0300

    Add Application level class with annotation hiltAndroidApp from hilt

---
+++ app/src/main/java/com/moemaair/lictionary/AppApplication.kt
@@ -0,0 +1,6 @@
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+@HiltAndroidApp
+class AppApplication: Application()
