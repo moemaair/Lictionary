2ec4dc777b86e598dd1019d34ab4211480a0baf9
MohamedIbrahim <rageali12@gmail.com>
2023-01-24 10:10:35 +0300

    Implementation of providers in Dependency injection with dagger hilt

--- app/src/main/java/com/moemaair/lictionary/feature_dictionary/di/AppModule.kt
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/di/AppModule.kt
@@ -1,14 +1,58 @@
 package com.moemaair.lictionary.feature_dictionary.di
 
+import Converters
+import GetWordInfo
+import GsonParser
+import WordInfoRepoImpl
+import android.app.Application
+import androidx.room.Room
+import com.google.gson.Gson
+import com.moemaair.lictionary.feature_dictionary.data.local.WordInfoDatabase
+import com.moemaair.lictionary.feature_dictionary.data.remote.LictionaryApi
+import com.moemaair.lictionary.feature_dictionary.domain.repository.WordInfoRepo
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideGetWordInfoUseCase(repository: WordInfoRepo): GetWordInfo {
+        return GetWordInfo(repository)
+    }
 
+    @Provides
+    @Singleton
+    fun provideWordInfoRepository(
+        db: WordInfoDatabase,
+        api: LictionaryApi
+    ): WordInfoRepo {
+        return WordInfoRepoImpl(api, db.dao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideWordInfoDatabase(app: Application): WordInfoDatabase {
+        return Room.databaseBuilder(
+            app, WordInfoDatabase::class.java, "word_db"
+        ).addTypeConverter(Converters(GsonParser(Gson())))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDictionaryApi(): LictionaryApi {
+        return Retrofit.Builder()
+            .baseUrl(LictionaryApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(LictionaryApi::class.java)
+    }
 
 }
