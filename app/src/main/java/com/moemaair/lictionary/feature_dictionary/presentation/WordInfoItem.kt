789214d02166c157a5b86b987f724266489fa0d1
MohamedIbrahim <rageali12@gmail.com>
2023-01-27 23:06:29 +0300

    capture inputted word and return the string that hold its meaning (s) and the word itself

--- app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/WordInfoItem.kt
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/WordInfoItem.kt
@@ -1,11 +1,46 @@
 package com.moemaair.lictionary.feature_dictionary.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.moemaair.lictionary.feature_dictionary.domain.model.WordInfo
 
 @Composable
 fun WordInfoItem(
-    wordInfo: WordInfo
+    wordInfo: WordInfo,
+    modifier: Modifier
 ) {
+
+    Column(modifier = modifier) {
+        Text(
+            text = wordInfo.word,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        Text(text = wordInfo.phonetic, fontWeight = FontWeight.Light)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = wordInfo.word)
+
+        wordInfo.meanings.forEach { meaning ->
+            Text(text = meaning.partOfSpeech, fontWeight = FontWeight.Bold)
+            meaning.definitions.forEachIndexed { i, definition ->
+                Text(text = "${i + 1}. ${definition.definition}")
+                Spacer(modifier = Modifier.height(8.dp))
+                definition.example?.let { example ->
+                    Text(text = "Example: $example")
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
     
 }
