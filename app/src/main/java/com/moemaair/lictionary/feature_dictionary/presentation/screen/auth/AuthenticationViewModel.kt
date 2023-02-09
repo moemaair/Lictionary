9b0fa406c0ea85e7cb61670162b367faeff8a1b5
MohamedIbrahim <rageali12@gmail.com>
2023-02-09 20:18:40 +0300

    Add method that will determine the value of the loadingState of the button on AuthenticationScreen

--- app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/screen/auth/AuthenticationViewModel.kt
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/screen/auth/AuthenticationViewModel.kt
@@ -1,5 +1,13 @@
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class AuthenticationViewModel : ViewModel(){
+    var loadingState = mutableStateOf(false)
+        private set
+
+    fun setLoadingState(loading: Boolean){
+        loadingState.value = loading
+    }
+
 
 }
