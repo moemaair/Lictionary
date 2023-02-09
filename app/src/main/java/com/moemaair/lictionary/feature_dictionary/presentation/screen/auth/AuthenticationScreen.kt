7adf628ac4275fa4d8d2e493fab6bdefee74e62b
MohamedIbrahim <rageali12@gmail.com>
2023-02-09 20:23:49 +0300

    Call Autheticationscreen's content inside AutheticationScreen

--- app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/screen/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/screen/auth/AuthenticationScreen.kt
@@ -1,8 +1,30 @@
 package com.moemaair.lictionary.feature_dictionary.presentation.screen.auth
 
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import com.stevdzasan.messagebar.ContentWithMessageBar
+import com.stevdzasan.messagebar.MessageBarState
+import com.stevdzasan.onetap.OneTapSignInState
 
 @Composable
-fun AuthenticationScreen() {
+fun AuthenticationScreen(
+    oneTapSignInState: OneTapSignInState, // library state for one tap google sign in
+    messageBarState: MessageBarState, // library state with predefined data- { Snackbar }
+    onDialogDismissed: (String) -> Unit,
+    onTokenReceived: (String) -> Unit,
+    loadingState: Boolean,
+    onButtonClick: () -> Unit
+) {
+    Scaffold(
+        content = {
+            ContentWithMessageBar(messageBarState = messageBarState) {
+                AuthenticationScreenContent(
+                    loadingState = loadingState,
+                    onButtonClick = onButtonClick
+                )
+            }
+        }
+    )
     
 }
