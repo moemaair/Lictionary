0e66f5b902979766349ac8b44141464d0ee92962
MohamedIbrahim <rageali12@gmail.com>
2023-02-09 21:02:27 +0300

    Add a navGraph composable to hold all the routes composables

---
+++ app/src/main/java/com/moemaair/lictionary/navigation/ScreenNavGraph.kt
@@ -0,0 +1,10 @@
+package com.moemaair.lictionary.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun ScreenNavGraph(startDestination: String, navController: NavHostController) {
+
+}
