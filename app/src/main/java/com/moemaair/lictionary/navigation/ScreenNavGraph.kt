11a378c3502d50b3d76f6c0b65265574c31b51fa
MohamedIbrahim <rageali12@gmail.com>
2023-02-09 21:03:37 +0300

    include a NavHost composable from navigation API

--- app/src/main/java/com/moemaair/lictionary/navigation/ScreenNavGraph.kt
+++ app/src/main/java/com/moemaair/lictionary/navigation/ScreenNavGraph.kt
@@ -3,8 +3,14 @@ package com.moemaair.lictionary.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
 fun ScreenNavGraph(startDestination: String, navController: NavHostController) {
+    NavHost(
+        startDestination = startDestination,
+        navController = navController
+    ){
 
+    }
 }
