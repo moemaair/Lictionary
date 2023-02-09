3d64c05590a615603af237c013e7643dac2563fb
MohamedIbrahim <rageali12@gmail.com>
2023-02-09 21:06:45 +0300

    call our authenticationScreen composable inside NavGraphBuilder extention function

--- app/src/main/java/com/moemaair/lictionary/navigation/ScreenNavGraph.kt
+++ app/src/main/java/com/moemaair/lictionary/navigation/ScreenNavGraph.kt
@@ -1,9 +1,14 @@
 package com.moemaair.lictionary.navigation
 
+import AuthenticationViewModel
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.moemaair.lictionary.feature_dictionary.presentation.screen.auth.AuthenticationScreen
 
 @Composable
 fun ScreenNavGraph(startDestination: String, navController: NavHostController) {
@@ -12,5 +17,22 @@ fun ScreenNavGraph(startDestination: String, navController: NavHostController) {
         navController = navController
     ){
 
+    }
+}
+
+//home graph
+
+fun NavGraphBuilder.authenticationScreen(){
+    composable(route = Screen.Authentication.route) {
+        val authenticationViewModel: AuthenticationViewModel = viewModel()
+        AuthenticationScreen(
+            oneTapSignInState = ,
+            messageBarState = ,
+            onDialogDismissed = ,
+            onTokenReceived = ,
+            loadingState =
+        ) {
+
+        }
     }
 }
