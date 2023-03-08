f296fe2f2a0cf57b2a942886a6cfe7baf5a7e1ac
moemaair <rageali12@gmail.com>
2023-03-08 10:57:11 +0300

    reference hidded values using Buildconfig

--- app/src/main/java/com/moemaair/lictionary/core/util/Constants.kt
+++ app/src/main/java/com/moemaair/lictionary/core/util/Constants.kt
@@ -1,9 +1,11 @@
 package com.moemaair.lictionary.core.util
 
+import com.moemaair.lictionary.BuildConfig
+
 object Constants {
     // From mongodb atlas of Project Lictionary
-//    const val APP_ID = "mylictionary-xvlpc"
-//    const val CLIENT_ID = "370067764709-d5nabk5gj2754rlhn6b8ndvojj405cj6.apps.googleusercontent.com"
+    const val APP_ID = BuildConfig.APP_ID
+    const val CLIENT_ID = BuildConfig.CLIENT_ID
 
 }
 
