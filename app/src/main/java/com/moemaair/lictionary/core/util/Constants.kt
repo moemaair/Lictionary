a38d8eaed7f7489f55e063ad3c43bd4717c71441
moemaair <rageali12@gmail.com>
2023-03-08 10:53:21 +0300

    added secret gradle plugin

--- app/src/main/java/com/moemaair/lictionary/core/util/Constants.kt
+++ app/src/main/java/com/moemaair/lictionary/core/util/Constants.kt
@@ -2,8 +2,8 @@ package com.moemaair.lictionary.core.util
 
 object Constants {
     // From mongodb atlas of Project Lictionary
-    const val APP_ID = "mylictionary-xvlpc"
-    const val CLIENT_ID = "370067764709-d5nabk5gj2754rlhn6b8ndvojj405cj6.apps.googleusercontent.com"
+//    const val APP_ID = "mylictionary-xvlpc"
+//    const val CLIENT_ID = "370067764709-d5nabk5gj2754rlhn6b8ndvojj405cj6.apps.googleusercontent.com"
 
 }
 
