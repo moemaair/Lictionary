9b61dcfa19a103b3177766ec97ca9617faa08115
moemaair <rageali12@gmail.com>
2023-12-09 00:33:41 +0300

    add onBoarding screen route

--- app/src/main/java/com/moemaair/lictionary/navigation/Screen.kt
+++ app/src/main/java/com/moemaair/lictionary/navigation/Screen.kt
@@ -2,4 +2,6 @@ package com.moemaair.lictionary.navigation
 sealed class Screen (val route: String){
     object Home: Screen(route = "Home")
     object Authentication : Screen(route = "Authentication")
+
+    object Onboarding : Screen(route = "Onboarding")
 }
