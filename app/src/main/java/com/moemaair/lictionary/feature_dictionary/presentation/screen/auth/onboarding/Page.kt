2e428fa93ad6d6d283cd8edfb5c4aaa56dabaeb5
moemaair <rageali12@gmail.com>
2023-12-08 20:41:41 +0300

    add list of page object with data in it i.e image and desc

--- app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/screen/auth/onboarding/Page.kt
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/screen/auth/onboarding/Page.kt
@@ -1,8 +1,24 @@
 package com.moemaair.lictionary.feature_dictionary.presentation.screen.auth.onboarding
 
 import androidx.annotation.DrawableRes
+import com.moemaair.lictionary.R
 
 data class Page(
     val description: String,
     @DrawableRes val image:Int
-)
+){
+    val pages: List<Page> = listOf(
+        Page(
+            description = "pager 1" ,
+            image  = R.drawable.dictionary_rafiki
+        ),
+        Page(
+            description = "pager 2",
+            image = R.drawable.dictionary_rafiki
+        ),
+        Page(
+            description = "pager 2",
+            image = R.drawable.dictionary_rafiki
+        ),
+    )
+}
