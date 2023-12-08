8d93b18e39d9ee25ca53d0a948d253f81d40e10b
moemaair <rageali12@gmail.com>
2023-12-08 20:38:52 +0300

    create a page class

---
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/screen/auth/onboarding/Page.kt
@@ -0,0 +1,8 @@
+package com.moemaair.lictionary.feature_dictionary.presentation.screen.auth.onboarding
+
+import androidx.annotation.DrawableRes
+
+data class Page(
+    val description: String,
+    @DrawableRes val image:Int
+)
