5295faf294d4fbecfbedb847e8ca2983a45f4128
moemaair <rageali12@gmail.com>
2023-12-09 00:34:46 +0300

    dummy lottie animations with logic class

---
+++ app/src/main/java/com/moemaair/lictionary/feature_dictionary/presentation/Search_with_ease_lottie.kt
@@ -0,0 +1,42 @@
+package com.moemaair.lictionary.feature_dictionary.presentation
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionResult
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.moemaair.lictionary.R
+
+@Composable
+fun Search_with_ease_lottie(
+
+) {
+    val preloaderLottieComposition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(
+           R.raw.searchwithease_lottie
+        )
+    )
+
+    val preloaderProgress by animateLottieCompositionAsState(
+        preloaderLottieComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+
+
+    LottieAnimation(
+        composition = preloaderLottieComposition,
+        progress = preloaderProgress,
+        modifier = Modifier
+    )
+
+
+
+}
